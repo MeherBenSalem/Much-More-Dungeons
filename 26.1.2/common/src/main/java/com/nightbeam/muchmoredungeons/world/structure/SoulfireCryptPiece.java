846eb5644ac828459ffd0b061cc6cca5c68615ff
NAIZO <74470806+MeherBenSalem@users.noreply.github.com>
2026-08-26 10:27:31 +0100

    Fix 26.x Soulfire Crypt build against Minecraft 26 entity API.
    Drop direct WitherSkeleton spawning in 26.1.2/26.2 pieces; keep chest loot markers and rely on the template entity for Cinder Warden.

--- 26.1.2/common/src/main/java/com/nightbeam/muchmoredungeons/world/structure/SoulfireCryptPiece.java
+++ 26.1.2/common/src/main/java/com/nightbeam/muchmoredungeons/world/structure/SoulfireCryptPiece.java
@@ -4,15 +4,9 @@ import com.nightbeam.muchmoredungeons.Constants;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.monster.WitherSkeleton;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Mirror;
@@ -25,7 +19,6 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.BlockIgnorePr
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.phys.AABB;
 
 public class SoulfireCryptPiece extends TemplateStructurePiece {
 
@@ -69,19 +62,8 @@ public class SoulfireCryptPiece extends TemplateStructurePiece {
                 container.setLootTable(LOOT, random.nextLong());
             }
         } else if ("guardian".equals(marker)) {
+            // Cinder Warden is embedded in the structure template entities list.
             level.setBlock(pos, Blocks.AIR.defaultBlockState(), 3);
-            if (level.getEntitiesOfClass(WitherSkeleton.class, new AABB(pos).inflate(2.0)).isEmpty()) {
-                WitherSkeleton warden = EntityType.WITHER_SKELETON.create(level.getLevel());
-                if (warden != null) {
-                    warden.moveTo(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5, random.nextFloat() * 360f, 0f);
-                    warden.setCustomName(Component.literal("Cinder Warden"));
-                    warden.setCustomNameVisible(true);
-                    warden.setPersistenceRequired();
-                    warden.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.STONE_SWORD));
-                    warden.setItemSlot(EquipmentSlot.CHEST, new ItemStack(Items.CHAINMAIL_CHESTPLATE));
-                    level.addFreshEntity(warden);
-                }
-            }
         }
     }
 }
